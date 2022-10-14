cbdc46d54b49fc97d6bf2a5a5d21e04bda8d9850
KralMarko123 <kralmarko123@gmail.com>
2022-10-14 21:33:12 +0200

    added to timer

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,29 +1,46 @@
 import { useState, useEffect } from "react";
 import { TimerProps } from "./TimerTypes";
+import "./Timer.css";
 
-const Timer = ({ start, increment }: TimerProps) => {
-	const [timeLeft, setTimeLeft] = useState<number>(0);
-
-	useEffect(() => {
-		const generateTimer = () => {
-			setTimeLeft(start);
-		};
-		generateTimer();
-	}, [start, increment]);
+const Timer = ({ start, decrement }: TimerProps) => {
+	const [timeLeft, setTimeLeft] = useState<number>(start);
+	const [decrementBy, setDecrementBy] = useState<number>(1);
 
 	useEffect(() => {
 		if (timeLeft === 0) return;
-
-		const interval = setInterval(() => {
-			increment ? setTimeLeft((prev) => prev - increment) : setTimeLeft((prev) => prev - 1);
-		}, 1000);
+		const interval = setInterval(
+			() => setTimeLeft((prev) => prev - decrementBy),
+			decrementBy * 1000
+		);
 
 		return () => {
 			clearInterval(interval);
 		};
-	}, [timeLeft]);
+	}, [timeLeft, decrementBy]);
 
-	return <div className="timer__container">{Math.floor(timeLeft)}</div>;
+	useEffect(() => {
+		const setTimer = () => {
+			setTimeLeft(start);
+			if (decrement && decrement > 0) {
+				setDecrementBy(decrement);
+			} else setDecrementBy(1);
+		};
+		setTimer();
+	}, [start, decrement]);
+
+	return (
+		<div className="timer">
+			<div className="timer__controls">
+				<span className="timer__time">{Math.floor(timeLeft)}</span>
+				<button className="timer__reset" onClick={() => setTimeLeft(start)}>
+					Reset Timer
+				</button>
+			</div>
+			<p className="timer__info">
+				{timeLeft > 0 ? `Decrementing by: ${decrementBy}s` : "Finished"}
+			</p>
+		</div>
+	);
 };
 
 export default Timer;
--- src/components/Timer/TimerTypes.ts
+++ src/components/Timer/TimerTypes.ts
@@ -1,4 +1,4 @@
 export type TimerProps = {
 	start: number;
-	increment?: number;
+	decrement?: number;
 };
